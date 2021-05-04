b177623adf6b0b680424f067284ccae236c006f7
pras75299 <singh.prashantking@gmail.com>
2021-05-05 01:27:22 +0530

    remove fa icon from appjs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
   const [people] = useState(data);
