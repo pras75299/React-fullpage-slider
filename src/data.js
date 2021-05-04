720a5bb95c22275d5447fdd60ae5a17c6a23f5d9
pras75299 <singh.prashantking@gmail.com>
2021-05-05 01:10:09 +0530

    add images url in data.js

--- src/data.js
+++ src/data.js
@@ -2,30 +2,26 @@ const people = [
   {
     id: 1,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      'https://images.pexels.com/photos/1334146/pexels-photo-1334146.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     name: 'maria ferguson',
-    title: 'office manager',
   },
   {
     id: 2,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      'https://images.pexels.com/photos/53754/pexels-photo-53754.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     name: 'john doe',
-    title: 'regular guy',
   },
   {
     id: 3,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
+      'https://images.pexels.com/photos/1040489/pexels-photo-1040489.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     name: 'peter smith',
-    title: 'product designer',
   },
   {
     id: 4,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      'https://images.pexels.com/photos/391994/pexels-photo-391994.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     name: 'susan andersen',
-    title: 'the boss',
   },
 ];
 
